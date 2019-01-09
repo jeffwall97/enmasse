8a05fde8fcb1904d70472d728e11cb7dce5ff59e
Jens Reimann <jreimann@redhat.com>
2019-01-09 12:51:03 +0100

    Use new API model

--- iot/tenant-service/src/main/java/io/enmasse/iot/model/IoTProjects.java
+++ iot/tenant-service/src/main/java/io/enmasse/iot/model/IoTProjects.java
@@ -5,24 +5,22 @@
 
 package io.enmasse.iot.model;
 
-import java.lang.reflect.Proxy;
+import static io.enmasse.iot.model.v1.IoTCrd.project;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.lang.reflect.Proxy;
 
 import io.enmasse.iot.model.v1.DoneableIoTProject;
 import io.enmasse.iot.model.v1.IoTProject;
 import io.enmasse.iot.model.v1.IoTProjectList;
+import io.enmasse.model.CustomResourceDefinitions;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 
 public final class IoTProjects {
 
-    private static final Logger logger = LoggerFactory.getLogger(IoTProjects.class);
-
     static {
-        logger.info("CRD: {}", IoTProject.CRD);
+        CustomResourceDefinitions.registerAll();
     }
 
     private IoTProjects() {
@@ -34,7 +32,7 @@ public final class IoTProjects {
 
     public static Client forClient(final KubernetesClient client) {
         final MixedOperation<IoTProject, IoTProjectList, DoneableIoTProject, Resource<IoTProject, DoneableIoTProject>> result = client
-                .customResources(IoTProject.CRD, IoTProject.class, IoTProjectList.class, DoneableIoTProject.class);
+                .customResources(project(), IoTProject.class, IoTProjectList.class, DoneableIoTProject.class);
 
         return (Client) Proxy.newProxyInstance(IoTProjects.class.getClassLoader(), new Class<?>[] { Client.class },
                 (proxy, method, args) -> {
